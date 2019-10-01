5705a9c64f326c6a901ef3eca7dea3627f945f24
Paris Mongkolkul <paris.kawmai@gmail.com>
2019-10-01 18:14:35 +0100

    push build for bug reason

---
+++ build/precache-manifest.bcf0312769b2a7445b62a135f934d957.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f092f2424771e30b64a9b2af0881f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61c3bd068b7b1962cec9",
+    "url": "/static/css/main.4d208887.chunk.css"
+  },
+  {
+    "revision": "c55ff4914777128670d1",
+    "url": "/static/js/2.abfa401e.chunk.js"
+  },
+  {
+    "revision": "61c3bd068b7b1962cec9",
+    "url": "/static/js/main.97f3c80d.chunk.js"
+  },
+  {
+    "revision": "b35a3eecf54ef9377f71",
+    "url": "/static/js/runtime~main.9bcb6baf.js"
+  },
+  {
+    "revision": "3acaa9a6e3eb5f5e2d183138c2cb727f",
+    "url": "/static/media/garena.3acaa9a6.png"
+  },
+  {
+    "revision": "af23b71da11ef0a38c8dad8a7bcddf95",
+    "url": "/static/media/github-logo.af23b71d.png"
+  },
+  {
+    "revision": "945c677e5cc8a3f26a4b9eac681214bb",
+    "url": "/static/media/my_resume.945c677e.pdf"
+  },
+  {
+    "revision": "70dcdcea87f55ffb4df42fd78959077f",
+    "url": "/static/media/resume_thumbnail.70dcdcea.png"
+  },
+  {
+    "revision": "03a648762dbd1ba73d853d79c01b8ce4",
+    "url": "/static/media/sertis_logo.03a64876.png"
+  },
+  {
+    "revision": "68745f12c0192fa912d532450467d8ae",
+    "url": "/static/media/thaisoc.68745f12.png"
+  }
+]);
